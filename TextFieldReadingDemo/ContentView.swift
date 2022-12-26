154a81e0602028793a7640cf4e9e5ea6ea8a45cb
Davide Troise <davide@troise.net>
2022-12-26 18:25:04 +0100

    Initial Commit

---
+++ TextFieldReadingDemo/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  TextFieldReadingDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextFieldReadingDemo/TextFieldReadingDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldReadingDemoApp.swift
+//  TextFieldReadingDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct TextFieldReadingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
